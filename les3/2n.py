e9e1a66b5b82d4e544d5995770c6582fccb08f48
Nikitosina <nikitarat1@gmail.com>
2019-01-20 11:50:16 +1000

    1 normal

---
+++ les3/2n.py
@@ -0,0 +1,16 @@
+# Задача-2:
+# Напишите функцию, сортирующую принимаемый список по возрастанию.
+# Для сортировки используйте любой алгоритм (например пузырьковый).
+# Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
+
+
+def sort_to_max(origin_list):
+    for i in range(len(origin_list)):
+        for j in range(len(origin_list) - 1):
+            if origin_list[j] > origin_list[j + 1]:
+                origin_list[j], origin_list[j + 1] = origin_list[j + 1], origin_list[j]
+
+    return origin_list
+
+
+print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
